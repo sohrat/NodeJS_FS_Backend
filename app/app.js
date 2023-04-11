1cb77c0290567cad8d601db12c227ef94b1a323f
iata-sohrat <shohrat.permanov.external@digitall.com>
2023-04-11 13:27:23 +0500

    Request Listener

---
+++ app/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+// use middleware to form our contract for incoming jon payloads ONLY!!!
+app.use(express.json());
+// use middleware for url encoding
+app.use(express.urlencoded({ extended: true }));
+// use middleware to handle cars police
+app.use(cors());
+
+// health point checker
+// http://localhost:3001
+app.get('/', (req, res, next) => {
+    res.status(200).json({ message: 'Service is up' });
+});
+
+// routers
+
+
+// bad url or error handler with middleware
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error:{
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const app = require('./app/app');
 require('dotenv').config();
 
-http.createServer().listen(process.env.port, () => {
+http.createServer(app).listen(process.env.port, () => {
     console.log(`Server running on port: ${process.env.port}`);
 });
