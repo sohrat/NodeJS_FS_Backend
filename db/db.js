15d71508c3ba591627e4cd62df7530da2c115678
iata-sohrat <shohrat.permanov.external@digitall.com>
2023-04-12 14:28:52 +0500

    Updated Router, New User, DB functionality

--- db/db.js
+++ db/db.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const User = require('../models/userModel');
 
 const connect = async () => {
     await mongoose.connect(process.env.mongo);
 };
 
-module.exports = { connect};
+const disconnect = async () => {
+    await mongoose.connection.close();
+};
+
+// obj {email: req.body.email}
+const findUser = async (obj) => {
+    User.findOne(obj)
+};
+
+const saveUser = async () => {
+    return await newUser.save();
+};
+
+module.exports = { connect, disconnect, findUser, saveUser };
 
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,64 +1,14 @@
-const express = require('express');
+const express =  require('express');
+const { saveUser } = require('../db/db');
+
 const router = express.Router();
 
-// returns response in url localhost:3001/users
-router.get('/', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Successfull - GET',
-        metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
+router.post('/register', (req, res, next) => {
+    //findUser if user exist return response
+    //'Email Exist try logging in'
+    //else encrypt the password set the password with encrypted password
+    //saveUser(newUser);
 });
-
-// returns response in url localhost:3001/users/123
-router.get('/:id', (req,res,next) =>{
-    res.status(200).json({
-        message: 'Successfull - GET by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
-
-// returns response in url localhost:3001/users
-router.post('/', (req, res, next) =>{
-    const name = req.body.name
-    res.status(201).json({
-        message: 'Successfull - POST',
-        metadata: {
-            name: name,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
-
-// returns response in url localhost:3001/users/123
-router.get('/:id', (req,res,next) =>{
-    res.status(200).json({
-        message: 'Successfull - PUT by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
-
-// returns response in url localhost:3001/users/123
-router.get('/:id', (req,res,next) =>{
-    res.status(200).json({
-        message: 'Successfull - DELETE by ID',
-        metadata: {
-            id: req.params.id,
-            hostname: req.hostname,
-            method: req.method,
-        },
-    });
-});
+router.post('/login', (req, res) => {});
 
 module.exports = router;
